c0e9cab13de7757902b0b8deb205e49837dea096
Daniel Rojo <danielrojo@gmail.com>
2021-09-24 11:42:53 +0200

    model trivial modification

--- src/app/model/cardmodel.ts
+++ src/app/model/cardmodel.ts
@@ -1,10 +1,12 @@
 export class CardModel {
     question = '';
     answers: string[] = [];
+    correctAnswer = '';
     constructor(jsonCard?: any) {
         if (jsonCard !== undefined) {
             this.question = jsonCard.question;
             this.answers = jsonCard.incorrect_answers;
+            this.correctAnswer = jsonCard.correct_answer;
             this.answers.push(jsonCard.correct_answer);
         }
     }
